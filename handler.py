3a4f9c7c2ac9db4d0cac186999c8b9be8c809c98
James Holloway <james.holloway94@gmail.com>
2026-04-28 18:19:37 +0000

    push mysql connection pool sync after refresh

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-28 17:25:27 UTC
+# Last sync: 2026-04-28 18:19:37 UTC
