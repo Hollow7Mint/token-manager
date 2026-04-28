27230a3513ba59ee4d66afcf68ce31341cbb41de
James Holloway <james.holloway94@gmail.com>
2026-04-28 21:49:37 +0000

    save bastion policy sync marker after review

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-28 20:24:15 UTC
+# Last sync: 2026-04-28 21:49:37 UTC
