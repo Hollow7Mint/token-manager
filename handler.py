b3e243e8e38768e761ab275e47591048a960cdbb
James Holloway <james.holloway94@gmail.com>
2026-05-01 13:50:36 +0000

    reload elb health check marker for nightly audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 13:02:59 UTC
+# Last sync: 2026-05-01 13:50:36 UTC
