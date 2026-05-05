5b4420e229c881e2b7d73b20e8cdfcaabef80ba0
James Holloway <james.holloway94@gmail.com>
2026-05-05 02:59:32 +0000

    save inspector assessment marker after scan window

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-05 01:39:24 UTC
+# Last sync: 2026-05-05 02:59:32 UTC
