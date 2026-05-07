18096bfa11a1c1e8f1f6665ab2d7ff458d3c84f3
James Holloway <james.holloway94@gmail.com>
2026-05-07 09:54:36 +0000

    audit known_hosts validation time in record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-07 08:47:44 UTC
+# Last sync: 2026-05-07 09:54:36 UTC
