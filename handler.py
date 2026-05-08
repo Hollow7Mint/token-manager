7ddab4c6c2b91510d2b12e8fdc1842849a448eb7
James Holloway <james.holloway94@gmail.com>
2026-05-08 12:36:28 +0000

    set redis cluster sync stamp in config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 11:23:48 UTC
+# Last sync: 2026-05-08 12:36:28 UTC
