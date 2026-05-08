4526989ca62a8ab7681376ee26a9424f2e2259d1
James Holloway <james.holloway94@gmail.com>
2026-05-08 17:32:30 +0000

    anchor ssh client config sync after key rotation

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 16:25:45 UTC
+# Last sync: 2026-05-08 17:32:30 UTC
