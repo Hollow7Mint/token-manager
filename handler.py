0326c0d6a11322b76719d9794e4401e3c6a6630c
James Holloway <james.holloway94@gmail.com>
2026-05-09 00:46:37 +0000

    run mongodb heartbeat for scheduled audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 23:53:03 UTC
+# Last sync: 2026-05-09 00:46:37 UTC
