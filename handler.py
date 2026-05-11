0b3df2347145be21a8e33979ead495e5626fafeb
James Holloway <james.holloway94@gmail.com>
2026-05-11 07:45:42 +0000

    rebuild nat gateway sync record after route change

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 06:30:17 UTC
+# Last sync: 2026-05-11 07:45:42 UTC
