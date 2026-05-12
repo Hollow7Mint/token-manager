796e3d0711c9cdac565525c739fed7a285a14c6c
James Holloway <james.holloway94@gmail.com>
2026-05-12 07:57:01 +0000

    finish mongodb sharding sync heartbeat after balance

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 07:01:50 UTC
+# Last sync: 2026-05-12 07:57:01 UTC
