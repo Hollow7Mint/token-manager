8580d0db88a92965dfbec1084c7db92686ac95e3
James Holloway <james.holloway94@gmail.com>
2026-05-12 01:47:45 +0000

    harvest postgres vacuum sync for nightly audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 00:09:04 UTC
+# Last sync: 2026-05-12 01:47:45 UTC
