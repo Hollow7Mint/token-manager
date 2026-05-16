61c944e7c1ff27b06678cd79e8a34b7b1b71ff9a
James Holloway <james.holloway94@gmail.com>
2026-05-16 14:57:20 +0000

    thread postgres wal sync through config banner

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 13:53:39 UTC
+# Last sync: 2026-05-16 14:57:20 UTC
