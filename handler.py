83f5da155c87fd7f608296c6aa453c1311b29da4
James Holloway <james.holloway94@gmail.com>
2026-05-16 01:57:18 +0000

    tag known_hosts sync checkpoint for key trail

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 00:30:59 UTC
+# Last sync: 2026-05-16 01:57:18 UTC
