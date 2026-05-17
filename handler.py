95a776487c8e384fa2e8b8ff9d1f08f989b26136
James Holloway <james.holloway94@gmail.com>
2026-05-17 15:43:47 +0000

    bump postgres schema sync stamp after migration

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-17 14:38:44 UTC
+# Last sync: 2026-05-17 15:43:47 UTC
