c5e9ff417a760d13e744684a6950f39fe78ffbb7
James Holloway <james.holloway94@gmail.com>
2026-05-19 17:19:47 +0000

    set ssh tunnel sync stamp in config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-19 16:48:34 UTC
+# Last sync: 2026-05-19 17:19:47 UTC
