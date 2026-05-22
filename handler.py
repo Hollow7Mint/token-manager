0233a54c3ffd328880b6fab5cc7543c755330091
James Holloway <james.holloway94@gmail.com>
2026-05-22 12:45:23 +0000

    mirror guardduty archive time in metadata record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-22 12:01:21 UTC
+# Last sync: 2026-05-22 12:45:23 UTC
