8d2d2e5be8b8110ca24dd3a8ff8d967051ee1ec2
James Holloway <james.holloway94@gmail.com>
2026-05-23 08:52:33 +0000

    recheck bastion connectivity sync after maintenance

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 07:17:22 UTC
+# Last sync: 2026-05-23 08:52:33 UTC
