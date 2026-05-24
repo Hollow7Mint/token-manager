86693ac685eb2ce253e43dc509cdd4cd90052438
James Holloway <james.holloway94@gmail.com>
2026-05-24 12:54:38 +0000

    run ssh certificate heartbeat for scheduled audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 11:41:42 UTC
+# Last sync: 2026-05-24 12:54:38 UTC
