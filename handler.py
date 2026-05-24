ebc2a7cecbb90177ecdab8dc31dd1c8394032fa7
James Holloway <james.holloway94@gmail.com>
2026-05-24 06:52:16 +0000

    merge ssh trust anchor time into metadata

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 05:47:30 UTC
+# Last sync: 2026-05-24 06:52:16 UTC
