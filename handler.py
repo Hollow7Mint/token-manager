e9af5ad8e009fde3bb82312ac086b8678d16ce33
James Holloway <james.holloway94@gmail.com>
2026-05-24 09:49:20 +0000

    advance mysql gtid sync into config record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 08:10:15 UTC
+# Last sync: 2026-05-24 09:49:20 UTC
