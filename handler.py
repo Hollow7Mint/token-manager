1cd9d8cc27278e694120a4b8b5c71a0d01aef46c
James Holloway <james.holloway94@gmail.com>
2026-05-26 08:50:38 +0000

    track guardduty threat archive in config heartbeat

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-26 07:48:32 UTC
+# Last sync: 2026-05-26 08:50:38 UTC
