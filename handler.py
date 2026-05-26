282b7c157b10d22fc46acb61bd71a39315fc7ae7
James Holloway <james.holloway94@gmail.com>
2026-05-26 15:58:45 +0000

    note postgres extension sync in config record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-26 14:42:56 UTC
+# Last sync: 2026-05-26 15:58:45 UTC
