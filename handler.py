658fd702f5f7b349ba2e3a6ae032b3ad60fd2721
James Holloway <james.holloway94@gmail.com>
2026-05-30 03:29:42 +0000

    advance cdk app deploy time into config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-30 02:41:33 UTC
+# Last sync: 2026-05-30 03:29:42 UTC
