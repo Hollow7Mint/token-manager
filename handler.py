32d92b9848ccf1a3e03d81d5c06c10ea20e99e56
James Holloway <james.holloway94@gmail.com>
2026-06-01 19:50:57 +0000

    resync parameter store path time for nightly audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-01 18:30:04 UTC
+# Last sync: 2026-06-01 19:50:57 UTC
