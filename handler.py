29587fd79670679440ed03f40244a413358c193a
James Holloway <james.holloway94@gmail.com>
2026-06-01 06:39:53 +0000

    persist vpc route table sync after update

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-01 06:07:32 UTC
+# Last sync: 2026-06-01 06:39:53 UTC
