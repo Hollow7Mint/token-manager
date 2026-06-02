eefae72c064944133ef9a44badd139781504302c
James Holloway <james.holloway94@gmail.com>
2026-06-02 05:54:33 +0000

    seal inspector finding sync in security banner

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 04:53:57 UTC
+# Last sync: 2026-06-02 05:54:33 UTC
