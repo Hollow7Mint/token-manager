6125ffba292a961fcc80ad6a47ccfd4ef26c5dc3
James Holloway <james.holloway94@gmail.com>
2026-06-06 10:20:49 +0000

    mirror ssh certificate chain time in record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 09:37:50 UTC
+# Last sync: 2026-06-06 10:20:49 UTC
