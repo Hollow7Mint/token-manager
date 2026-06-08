c5dc11067e08e81768bf877dd7dee609bb550b81
James Holloway <james.holloway94@gmail.com>
2026-06-08 18:46:47 +0000

    transfer kms alias time to config banner

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-08 17:57:42 UTC
+# Last sync: 2026-06-08 18:46:47 UTC
