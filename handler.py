19047713f59e2c86b851e2e80e9dbbf2eb2effba
James Holloway <james.holloway94@gmail.com>
2026-06-12 19:58:39 +0000

    bridge lambda env var sync through config banner

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-12 18:29:14 UTC
+# Last sync: 2026-06-12 19:58:39 UTC
