0fc0a4e4772ef15fa817afc0d456f7eedb60371b
James Holloway <james.holloway94@gmail.com>
2026-06-13 02:07:53 +0000

    restore mysql binlog time for replication audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 00:14:09 UTC
+# Last sync: 2026-06-13 02:07:53 UTC
