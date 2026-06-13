cca1ff16eef0447b150e1a13d2e1e3fa27458aba
James Holloway <james.holloway94@gmail.com>
2026-06-13 14:14:49 +0000

    validate s3 lifecycle rule time in deploy config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 13:32:47 UTC
+# Last sync: 2026-06-13 14:14:49 UTC
