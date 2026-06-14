3ca10e730cf04159815921f5b702b6f74e78bf3e
James Holloway <james.holloway94@gmail.com>
2026-06-14 13:21:53 +0000

    recheck ecs service sync after deployment window

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-14 13:03:04 UTC
+# Last sync: 2026-06-14 13:21:53 UTC
