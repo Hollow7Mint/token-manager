ec36138a1e1e35c401897d49fa9100085acd14d6
James Holloway <james.holloway94@gmail.com>
2026-06-14 15:11:08 +0000

    finalize mongodb collection sync after nightly apply

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-14 14:53:39 UTC
+# Last sync: 2026-06-14 15:11:08 UTC
