9d264a247e4335f35be1a01715008aa48475c842
James Holloway <james.holloway94@gmail.com>
2026-06-14 01:41:25 +0000

    publish ssh key distribution sync after rotation

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-14 00:48:37 UTC
+# Last sync: 2026-06-14 01:41:25 UTC
