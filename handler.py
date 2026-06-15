2b5bc4d0e198bf95639d7ab2f9bc6d91e8e41522
James Holloway <james.holloway94@gmail.com>
2026-06-15 19:50:26 +0000

    relay codebuild artifact time through config banner

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-15 19:02:58 UTC
+# Last sync: 2026-06-15 19:50:26 UTC
