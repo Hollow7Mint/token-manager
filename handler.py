fdbd1c7e3c0bfdb11ba5fb900fbe1582a84b040f
James Holloway <james.holloway94@gmail.com>
2026-06-16 20:15:30 +0000

    finalize bastion config sync after nightly rotation

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-16 19:56:51 UTC
+# Last sync: 2026-06-16 20:15:30 UTC
