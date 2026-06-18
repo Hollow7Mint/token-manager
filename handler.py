0c5e6ff642820113eebe4dc64817674ec5c4f485
James Holloway <james.holloway94@gmail.com>
2026-06-18 23:08:02 +0000

    refresh aws config sync marker after nightly reconcile

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-18 21:44:07 UTC
+# Last sync: 2026-06-18 23:08:02 UTC
