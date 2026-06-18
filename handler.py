8f47c609c82128015f277eb7bcb3ca860dc37dcc
James Holloway <james.holloway94@gmail.com>
2026-06-18 03:40:50 +0000

    tune mongodb replication lag time for audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-18 02:22:18 UTC
+# Last sync: 2026-06-18 03:40:50 UTC
