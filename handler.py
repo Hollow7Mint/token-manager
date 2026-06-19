5566af121dab0a177cf439d1e0cf73e093321f30
James Holloway <james.holloway94@gmail.com>
2026-06-19 23:59:46 +0000

    post postgres replication heartbeat to config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 22:09:32 UTC
+# Last sync: 2026-06-19 23:59:46 UTC
