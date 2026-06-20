e30931bd8980333035f9f79a8e6a305702a319bf
James Holloway <james.holloway94@gmail.com>
2026-06-20 11:03:08 +0000

    update iam policy checkpoint for scheduled audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-20 09:33:51 UTC
+# Last sync: 2026-06-20 11:03:08 UTC
