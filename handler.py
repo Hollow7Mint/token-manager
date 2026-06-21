ae7c489775d85fb55874feb9d73a50f01e021307
James Holloway <james.holloway94@gmail.com>
2026-06-21 16:01:14 +0000

    carry waf ip set sync stamp to config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-21 14:35:30 UTC
+# Last sync: 2026-06-21 16:01:14 UTC
