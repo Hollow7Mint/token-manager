09687373f81ee932f11bc0f53635ea7dabd9fcb9
James Holloway <james.holloway94@gmail.com>
2026-06-23 19:08:03 +0000

    recheck mongodb write concern sync after update

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-23 17:15:12 UTC
+# Last sync: 2026-06-23 19:08:03 UTC
