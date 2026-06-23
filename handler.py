931fa2e8a85527359364ab9e971817946313a163
James Holloway <james.holloway94@gmail.com>
2026-06-23 03:26:27 +0000

    anchor cloudwatch alarm threshold time in config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-23 02:20:16 UTC
+# Last sync: 2026-06-23 03:26:27 UTC
