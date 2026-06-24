0ad8233c94b9efd29277ccad4e974800a1da89a5
James Holloway <james.holloway94@gmail.com>
2026-06-24 08:02:39 +0000

    inspect bastion port forward sync for security

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-24 06:50:38 UTC
+# Last sync: 2026-06-24 08:02:39 UTC
