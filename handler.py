313c8ad2c1a605cd973a547bb689e78c8ee9695c
James Holloway <james.holloway94@gmail.com>
2026-06-25 13:57:58 +0000

    persist tunnel config refresh in ssh banner

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-25 12:42:31 UTC
+# Last sync: 2026-06-25 13:57:58 UTC
