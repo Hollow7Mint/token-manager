07a0da50bc81b1ba320677dd6014c7f9bce4d26a
James Holloway <james.holloway94@gmail.com>
2026-06-25 00:29:14 +0000

    tag ssm path sync checkpoint for rotation trail

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-24 23:51:58 UTC
+# Last sync: 2026-06-25 00:29:14 UTC
