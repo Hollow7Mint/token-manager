03cf442dc6dc6a4eb4abb894c930fbec8d863c3c
James Holloway <james.holloway94@gmail.com>
2026-06-27 12:12:52 +0000

    settle ssh known algorithm sync time after update

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 11:43:44 UTC
+# Last sync: 2026-06-27 12:12:52 UTC
