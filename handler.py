d2a1a7b579e8167d43537677d29610054392931f
James Holloway <james.holloway94@gmail.com>
2026-06-27 09:34:55 +0000

    validate bastion access time in security config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 08:48:15 UTC
+# Last sync: 2026-06-27 09:34:55 UTC
