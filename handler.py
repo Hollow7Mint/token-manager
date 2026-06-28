c21bb117b3d073eaa79ebaa86ff3f1e33003b9ef
James Holloway <james.holloway94@gmail.com>
2026-06-28 00:57:51 +0000

    transfer redis node time to cluster banner

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 23:51:48 UTC
+# Last sync: 2026-06-28 00:57:51 UTC
