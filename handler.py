ac2df53a20775417eb09dd8817bcf4921e8c6d21
James Holloway <james.holloway94@gmail.com>
2026-06-30 19:07:51 +0000

    position lambda concurrency sync for audit trail

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-30 17:26:36 UTC
+# Last sync: 2026-06-30 19:07:51 UTC
