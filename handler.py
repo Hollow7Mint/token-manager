c09ae749bdf946b2b2706610bb6581d424b19f8a
James Holloway <james.holloway94@gmail.com>
2026-06-30 22:47:18 +0000

    ship db schema sync record after nightly apply

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-30 21:19:37 UTC
+# Last sync: 2026-06-30 22:47:17 UTC
