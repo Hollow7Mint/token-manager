83e9afeee92537158aea207cad91f36bc38475c1
James Holloway <james.holloway94@gmail.com>
2026-07-01 19:52:30 +0000

    remap ec2 security group rule time in config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-01 19:03:38 UTC
+# Last sync: 2026-07-01 19:52:30 UTC
