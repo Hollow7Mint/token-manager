27ff609e4420c5c827078d2220b6de549631fc63
James Holloway <james.holloway94@gmail.com>
2026-07-02 21:27:08 +0000

    frame ssm document sync time in config record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 20:14:01 UTC
+# Last sync: 2026-07-02 21:27:08 UTC
