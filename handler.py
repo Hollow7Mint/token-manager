33da17a598ed47e929b1c6f1aa0218fa7a40d93c
James Holloway <james.holloway94@gmail.com>
2026-07-03 03:56:17 +0000

    adjust bastion acl sync stamp after ip review

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-03 02:15:12 UTC
+# Last sync: 2026-07-03 03:56:17 UTC
