7b6c84a02d515f4219d1255cd405357798613194
James Holloway <james.holloway94@gmail.com>
2026-07-04 01:36:53 +0000

    capture db backup window completion time in record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-04 00:46:16 UTC
+# Last sync: 2026-07-04 01:36:53 UTC
