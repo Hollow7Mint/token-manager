e411509a9aa65729a1705d583c494b554c74f6e8
James Holloway <james.holloway94@gmail.com>
2026-07-05 11:17:37 +0000

    publish lambda alias promotion marker after release

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-05 10:28:54 UTC
+# Last sync: 2026-07-05 11:17:37 UTC
