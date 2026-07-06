ead9e2e57f7384869a0898d3b030224ae05b442c
James Holloway <james.holloway94@gmail.com>
2026-07-06 03:43:25 +0000

    preserve mysql user permission sync in metadata

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-06 02:13:38 UTC
+# Last sync: 2026-07-06 03:43:25 UTC
