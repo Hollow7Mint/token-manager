2ad8299e9a5157bdca954c94f79b7ac66bf4734a
James Holloway <james.holloway94@gmail.com>
2026-07-06 02:13:38 +0000

    queue cloudfront distribution stamp after invalidation

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-06 01:53:48 UTC
+# Last sync: 2026-07-06 02:13:38 UTC
