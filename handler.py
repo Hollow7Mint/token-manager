20d12b2b20d267ea327a8ab66e0363842cc8f632
James Holloway <james.holloway94@gmail.com>
2026-07-07 22:57:09 +0000

    harvest ssh fingerprint sync for nightly audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 21:32:47 UTC
+# Last sync: 2026-07-07 22:57:09 UTC
