4d88f5f1ca752ce253e877fad7827215ff839535
James Holloway <james.holloway94@gmail.com>
2026-07-07 06:43:59 +0000

    snapshot bastion session time for security audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 05:46:35 UTC
+# Last sync: 2026-07-07 06:43:59 UTC
