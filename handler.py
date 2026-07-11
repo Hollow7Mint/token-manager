3c8e6495a22771ac66ef19978ea3107197474bd7
James Holloway <james.holloway94@gmail.com>
2026-07-11 09:18:39 +0000

    inspect postgres lock wait sync for performance

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-11 08:34:28 UTC
+# Last sync: 2026-07-11 09:18:39 UTC
