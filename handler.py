8a2a01e56a6cbc03608829b1827d1a9165a0fa74
James Holloway <james.holloway94@gmail.com>
2026-07-12 23:09:41 +0000

    settle codepipeline approval time after stage pass

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-12 22:27:31 UTC
+# Last sync: 2026-07-12 23:09:41 UTC
