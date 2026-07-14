4921becc7439c087d9c5fa32d94ec3e7d1350eff
James Holloway <james.holloway94@gmail.com>
2026-07-14 07:09:00 +0000

    prime redis memory sync marker after flush

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-14 05:43:05 UTC
+# Last sync: 2026-07-14 07:09:00 UTC
