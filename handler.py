b9435253eec3a75839611a1102753740532f4031
James Holloway <james.holloway94@gmail.com>
2026-07-15 13:21:19 +0000

    inject redis eviction sync marker into config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-15 12:09:16 UTC
+# Last sync: 2026-07-15 13:21:19 UTC
