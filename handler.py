5b528be24507d0d7b1272349abcd133b535b8b34
James Holloway <james.holloway94@gmail.com>
2026-07-17 16:10:00 +0000

    stamp inspector scan completion in security record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-17 16:01:17 UTC
+# Last sync: 2026-07-17 16:10:00 UTC
