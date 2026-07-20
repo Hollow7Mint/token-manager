db02092ce2e2abf2feca2008750171258c307c16
James Holloway <james.holloway94@gmail.com>
2026-07-20 04:08:40 +0000

    restore ecr scan result time for image audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-20 03:07:11 UTC
+# Last sync: 2026-07-20 04:08:40 UTC
