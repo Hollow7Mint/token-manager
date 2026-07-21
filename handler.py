750576f884084dd2dc0a7cbf0c3f088b6e35f692
James Holloway <james.holloway94@gmail.com>
2026-07-21 19:21:01 +0000

    inject ecs task definition marker into config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-21 19:08:23 UTC
+# Last sync: 2026-07-21 19:21:00 UTC
