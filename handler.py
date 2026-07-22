93e9f299f298bb890a8780982f8cd3ca7ffaa06c
James Holloway <james.holloway94@gmail.com>
2026-07-22 23:33:28 +0000

    annotate mysql slow log sync time for audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 22:49:46 UTC
+# Last sync: 2026-07-22 23:33:28 UTC
