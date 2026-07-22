43155f6268538c909f2ce95c3658d846e52a4aff
James Holloway <james.holloway94@gmail.com>
2026-07-22 20:38:58 +0000

    retain redis aof sync stamp for persistence audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 19:20:44 UTC
+# Last sync: 2026-07-22 20:38:58 UTC
