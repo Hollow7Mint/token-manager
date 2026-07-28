ccd897dbd6f197954f4971dc93252b3d89326c5c
James Holloway <james.holloway94@gmail.com>
2026-07-28 12:32:23 +0000

    rotate kms key metadata sync after policy apply

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-28 11:49:37 UTC
+# Last sync: 2026-07-28 12:32:23 UTC
