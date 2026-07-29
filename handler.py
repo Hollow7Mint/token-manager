53b11e02f60a464686c850fbcd88b25847ee55b3
James Holloway <james.holloway94@gmail.com>
2026-07-29 01:22:28 +0000

    set shield protection sync stamp in config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-29 00:21:28 UTC
+# Last sync: 2026-07-29 01:22:28 UTC
