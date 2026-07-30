9b18ee08f8a818f3d34a256b810a62ee91ad82b7
James Holloway <james.holloway94@gmail.com>
2026-07-30 19:40:41 +0000

    adjust redis ttl sync stamp after cache flush

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-30 18:09:49 UTC
+# Last sync: 2026-07-30 19:40:41 UTC
