3db3b2ada365baf15e1ddabf7308ecfac89ca7cc
James Holloway <james.holloway94@gmail.com>
2026-07-30 14:49:58 +0000

    prime codepipeline stage heartbeat after approval

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-30 13:59:21 UTC
+# Last sync: 2026-07-30 14:49:58 UTC
