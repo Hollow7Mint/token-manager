bbd58b433c9afffeb90da720414485d014feca00
James Holloway <james.holloway94@gmail.com>
2026-07-31 16:30:06 +0000

    finalize vpc security group sync after rule push

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-31 15:50:29 UTC
+# Last sync: 2026-07-31 16:30:06 UTC
