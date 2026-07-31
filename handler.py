4c7ff429d6b0e6b7d3732d483c0a5a642df7017e
James Holloway <james.holloway94@gmail.com>
2026-07-31 23:08:32 +0000

    touch redis sentinel sync during scheduled refresh

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-31 21:28:26 UTC
+# Last sync: 2026-07-31 23:08:32 UTC
