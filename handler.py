104082188326010b7254be9b3c29fab9666d8c6e
James Holloway <james.holloway94@gmail.com>
2026-08-02 04:17:16 +0000

    surface codebuild log sync time for pipeline audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-02 03:46:11 UTC
+# Last sync: 2026-08-02 04:17:16 UTC
