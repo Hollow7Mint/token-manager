8372a41b1bb2893ad4b55169cbf5473134e91fc6
James Holloway <james.holloway94@gmail.com>
2026-08-06 04:37:55 +0000

    prime bastion connection sync marker after refresh

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-06 03:27:37 UTC
+# Last sync: 2026-08-06 04:37:55 UTC
