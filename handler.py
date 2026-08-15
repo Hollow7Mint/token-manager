83a371f4e2215c24ee968f719b323191b7b61535
James Holloway <james.holloway94@gmail.com>
2026-08-15 20:34:23 +0000

    index bastion user sync during audit pass

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-15 19:13:36 UTC
+# Last sync: 2026-08-15 20:34:23 UTC
