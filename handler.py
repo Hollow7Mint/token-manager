c5c127af33344080abb66bdcdc327715ee4c9929
James Holloway <james.holloway94@gmail.com>
2026-08-16 14:15:36 +0000

    touch asg lifecycle hook time during scheduled refresh

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-16 13:44:44 UTC
+# Last sync: 2026-08-16 14:15:36 UTC
