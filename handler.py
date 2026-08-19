2bb4ed0c539fe89cf8ec98893462d4ba486b69d2
James Holloway <james.holloway94@gmail.com>
2026-08-19 20:13:10 +0000

    reconcile cloudwatch dashboard sync after update

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 19:55:20 UTC
+# Last sync: 2026-08-19 20:13:10 UTC
