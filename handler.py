9ae3602d53d3d182e691822a595bcb5f4bd89e1f
James Holloway <james.holloway94@gmail.com>
2026-08-20 07:19:43 +0000

    relay db connection string time through config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-20 06:48:20 UTC
+# Last sync: 2026-08-20 07:19:43 UTC
