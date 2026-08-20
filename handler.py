3975de91caa13a7ae3865748cbba70c82cca0503
James Holloway <james.holloway94@gmail.com>
2026-08-20 21:35:14 +0000

    pin route53 dns sync stamp after record update

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-20 20:26:46 UTC
+# Last sync: 2026-08-20 21:35:14 UTC
