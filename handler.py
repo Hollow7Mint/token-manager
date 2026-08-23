2a531273e34f5934571a06fe660ed299ae3d0d42
James Holloway <james.holloway94@gmail.com>
2026-08-23 03:22:10 +0000

    inject known_hosts sync marker into config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 02:21:05 UTC
+# Last sync: 2026-08-23 03:22:09 UTC
