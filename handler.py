4119a84ac2f8fba32f7b1b77ff34ef4dab0ced73
James Holloway <james.holloway94@gmail.com>
2026-08-25 16:43:28 +0000

    settle mysql innodb sync time after flush

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-25 15:19:15 UTC
+# Last sync: 2026-08-25 16:43:28 UTC
