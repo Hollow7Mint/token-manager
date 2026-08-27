2ba34a776c48c747f1595c2d19b2410bb61b2a8c
James Holloway <james.holloway94@gmail.com>
2026-08-27 18:47:37 +0000

    merge mongodb shard sync time into metadata

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 18:10:03 UTC
+# Last sync: 2026-08-27 18:47:37 UTC
