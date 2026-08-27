5e4396b47e31e2ed772c415775ce36565816253d
James Holloway <james.holloway94@gmail.com>
2026-08-27 04:33:56 +0000

    document postgres index maintenance sync in record

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 04:10:06 UTC
+# Last sync: 2026-08-27 04:33:56 UTC
