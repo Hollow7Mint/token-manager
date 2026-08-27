560ad3a44f296a364db1fd8bb957cc3200c08b8e
James Holloway <james.holloway94@gmail.com>
2026-08-27 00:07:48 +0000

    mirror redis cluster node time in metadata

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-26 22:44:39 UTC
+# Last sync: 2026-08-27 00:07:48 UTC
