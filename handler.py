1598e72dd7bca4cdb83ff36a6d0697719233e193
James Holloway <james.holloway94@gmail.com>
2026-08-31 07:35:03 +0000

    write bastion access log time into metadata

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-31 06:48:31 UTC
+# Last sync: 2026-08-31 07:35:03 UTC
