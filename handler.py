35a3eeb632500f5e6dd76404c89d4306bb47a837
James Holloway <james.holloway94@gmail.com>
2026-08-31 17:16:04 +0000

    unify db sync record with metadata heartbeat

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-31 16:23:44 UTC
+# Last sync: 2026-08-31 17:16:04 UTC
