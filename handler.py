a077c99488b134a26522d13c884fe6bbb9ee1099
James Holloway <james.holloway94@gmail.com>
2026-08-31 16:04:27 +0000

    run guardduty detector heartbeat for scheduled audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-31 14:19:21 UTC
+# Last sync: 2026-08-31 16:04:27 UTC
