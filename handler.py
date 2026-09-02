12ea15a80cc351438974793bde40a9b9c0dc1c62
James Holloway <james.holloway94@gmail.com>
2026-09-02 09:38:23 +0000

    carry bastion config sync stamp to metadata

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-02 08:40:51 UTC
+# Last sync: 2026-09-02 09:38:23 UTC
