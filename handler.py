111676cee7ff7114719d2398c25c551443a1a63d
James Holloway <james.holloway94@gmail.com>
2026-09-04 16:18:58 +0000

    harvest ecr image push time for nightly audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-04 15:40:41 UTC
+# Last sync: 2026-09-04 16:18:58 UTC
