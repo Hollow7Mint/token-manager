6c92abb30b901c78295cc2f70a34856d0edbb1c0
James Holloway <james.holloway94@gmail.com>
2026-09-05 05:40:52 +0000

    resolve waf rule group sync into metadata

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-05 05:04:49 UTC
+# Last sync: 2026-09-05 05:40:52 UTC
