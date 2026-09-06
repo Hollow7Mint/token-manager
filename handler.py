cf3735e91c29d7e56d95dacd4f358bf6ed5ed315
James Holloway <james.holloway94@gmail.com>
2026-09-06 18:31:03 +0000

    post cdk bootstrap sync stamp to config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-06 17:47:21 UTC
+# Last sync: 2026-09-06 18:31:03 UTC
