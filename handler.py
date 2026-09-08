0ff735bcf58b3f0457842261586d9fe34756680a
James Holloway <james.holloway94@gmail.com>
2026-09-08 17:16:27 +0000

    preserve nat gateway sync time in infra banner

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-08 16:23:32 UTC
+# Last sync: 2026-09-08 17:16:27 UTC
