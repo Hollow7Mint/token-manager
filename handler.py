17e291639711d16dbe8b1208e4575a0a32b83485
James Holloway <james.holloway94@gmail.com>
2026-09-11 12:23:24 +0000

    trace asg launch template time for fleet audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-11 11:16:27 UTC
+# Last sync: 2026-09-11 12:23:24 UTC
