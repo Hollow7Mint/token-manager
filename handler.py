a0953ac614bcb41922ea3258a26f0688e3d1c52f
James Holloway <james.holloway94@gmail.com>
2026-09-11 04:08:21 +0000

    reload bastion session recording sync for audit

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-11 03:02:00 UTC
+# Last sync: 2026-09-11 04:08:21 UTC
