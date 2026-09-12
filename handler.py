a1d29f592d11ce9291f4626a953ca978e5aa790b
James Holloway <james.holloway94@gmail.com>
2026-09-12 15:56:45 +0000

    validate mysql replication time in cluster config

--- handler.py
+++ handler.py
@@ -129,4 +129,4 @@ class TokenHandler:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-12 15:04:42 UTC
+# Last sync: 2026-09-12 15:56:45 UTC
